bac1da4fec55d01717562a42a33d7a9437ad6cf6
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-18 18:08:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,4 +4,16 @@ class Dog
     @name = name
     @breed = breed
   end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def breed(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end    
 end
